be93514f0512ba4e686a5a4ea796e39b200b5c17
Roman Yakubovich <r.yakubovich14@gmail.com>
2016-03-14 21:35:03 +0000

    1.9 Correct

--- hello.py
+++ hello.py
@@ -5,6 +5,5 @@ def application(env, start_response):
   ]
   body = env['QUERY_STRING'].split('&')
   body = [item + "\r\n" for item in body]
-  body = body[:len(body)-4]
   start_response(status, headers)
-  return [ body ]
+  return body 
