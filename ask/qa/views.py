cbe5ec47d50847c94635f04ae7fdd39c2b880349
Roman Yakubovich <r.yakubovich14@gmail.com>
2016-03-19 20:52:27 +0000

    2.5 Views Up

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -19,7 +19,7 @@ def main(request):
 		page = paginator.page(page)
 	except EmptyPage:
 		page = paginator.page(paginator.num_pages)
-	return render(request, "templates/mainpage.html", {
+	return render(request, "templates/main_page.html", {
 		'questions': page.object_list,
 		'paginator': paginator, 
 		'page': page,
