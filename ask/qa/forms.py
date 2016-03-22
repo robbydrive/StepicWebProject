39783eceebfb28ef92f4f68c8bdf7c3258d208bf
Roman Yakubovich <r.yakubovich14@gmail.com>
2016-03-22 20:05:29 +0000

    2.7 Auth

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -47,3 +47,46 @@ class AnswerForm(forms.Form):
 		answet.author = "Romain"
 		answer.save()
 		return answer
+
+class SignupForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField()
+	email = forms.EmailField()
+
+	def clean_username(self):
+		return username
+	
+	def clean_password(self):
+		return password
+	
+	def clean_email(self):
+		return email
+
+	def clean(self):
+		return self.cleaned_data
+
+	def save(self):
+		user = User.objects.create_user(self.cleaned_data['username'], self.cleaned_data['password)'])
+		user.email = self.cleaned_data(['email'])
+		user.save()
+		return user
+
+class LoginForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField()
+
+	def clean_username(self):
+		return username
+
+	def clean_password(self):
+		return password
+
+	def clean(self):
+		return self.cleaned_data
+
+	def save(self):
+		user = authenticate(username = self.cleaned_data['username'], password = self.cleaned_data['password'])
+		if user:
+			return user
+		else:
+			raise ValidationError('invalid username/password')
